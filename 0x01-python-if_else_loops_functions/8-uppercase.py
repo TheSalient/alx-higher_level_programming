4e927a6796a143926e1d9dbe61e456d8456a4806
TheSalient <abdulazeezlawal236@gmail.com>
2022-05-31 20:49:05 +0100

    last correction for the day.

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-	"""this function prints a string in uppercase followed by a new line."""
-	for i in str:
-		num = ord(i)
-		if num > 90:
-			num = num - 32
-			print("{:c}".format(num), end="")
-		else:
-			print("{:c}".format(num), end="")
-	print()
+    """this function prints a string in uppercase followed by a new line."""
+    for i in str:
+        num = ord(i)
+        if num > 90:
+            num = num - 32
+            print("{:c}".format(num), end="")
+        else:
+            print("{:c}".format(num), end="")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-	"""This function prints the last digit of a number."""
-	if number < 0:
-		number1 = number * -1
-		number2 = number1 % 10
-	else:
-		number1 = number * 1
-		number2 = number1 % 10
-		return (number2)
+    """This function prints the last digit of a number."""
+    if number < 0:
+        number1 = number * -1
+        number2 = number1 % 10
+    else:
+        number1 = number * 1
+        number2 = number1 % 10
+        return (number2)
