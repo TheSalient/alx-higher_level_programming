6a88a8932b7d25f1ec94d3feea19da230777a1ff
TheSalient <abdulazeezlawal236@gmail.com>
2022-06-01 13:29:26 +0100

    corrected file number 9.

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,10 @@ def uppercase(str):
     """this function prints a string in uppercase followed by a new line."""
     for i in str:
         num = ord(i)
-        if num > 90:
+        if num >= 97 and num <= 122:
             num = num - 32
-            print("{:c}".format(num), end="")
-        else:
-            print("{:c}".format(num), end="")
+        print("{:c}".format(num), end="")
+    print()
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
