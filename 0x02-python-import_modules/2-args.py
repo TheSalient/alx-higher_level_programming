ea27669d6dc04bd3804725218ea1844a1719808b
TheSalient <abdulazeezlawal236@gmail.com>
2022-06-02 17:49:58 +0100

    made correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     the = len(sys.argv)
     if the == 1:
         print("0 arguments.")
+    elif the == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(the - 1))
         for i in range(1, the):
