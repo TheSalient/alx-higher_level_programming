1914476fbdb5c9acfa9aa851dd4f7cddcd277dc4
TheSalient <abdulazeezlawal236@gmail.com>
2022-06-02 20:59:14 +0100

    i added a file.

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    sys.path.append("variable_load_5")
-    import variable_load_5 as i
-    print(i.a)
+    import variable_load_5 as a
+    print("{}".format(a))
