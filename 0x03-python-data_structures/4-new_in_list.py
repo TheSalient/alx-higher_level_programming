7fb76680e84d57e7a873af2d264e37a16ae0e904
TheSalient <abdulazeezlawal236@gmail.com>
2022-06-06 09:03:52 +0100

    corrected

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,5 +3,7 @@ def new_in_list(my_list, idx, element):
     for i in my_list:
         copList.append(i)
     if idx >= 0 or idx < len(my_list):
-       copList[idx] = element
-    return copList
+        copList[idx] = element
+        return copList
+    else:
+        return my_list
