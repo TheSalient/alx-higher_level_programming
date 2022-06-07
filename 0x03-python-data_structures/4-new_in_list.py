215f5dcd28ce96264730e197d860e4008319f32a
TheSalient <abdulazeezlawal236@gmail.com>
2022-06-07 10:54:34 +0100

    corrected

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,17 @@
 def new_in_list(my_list, idx, element):
-    copList = []
-    for i in my_list:
-        copList.append(i)
-    if idx >= 0 or idx < len(my_list):
+    if my_list is None:
+        pass
+    elif idx >= 0 or idx < len(my_list):
+        copList = my_list[:]
         copList[idx] = element
         return copList
+    else:
+        return my_list
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
