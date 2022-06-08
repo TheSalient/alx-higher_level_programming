9d7f1e8d6a19698a84d87ef89fc9937738700be3
TheSalient <abdulazeezlawal236@gmail.com>
2022-06-08 21:56:39 +0100

    maximum value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    fin_max = max(a_dictionary, key=a_dictionary.get)
-    return fin_max
+    if a_dictionary:
+        fin_max = max(a_dictionary, key=a_dictionary.get)
+        return fin_max
+    else:
+        return (None)
