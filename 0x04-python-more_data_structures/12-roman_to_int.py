a9f6b8012cc3aebc06bb4ad2ee98720fddf20205
TheSalient <abdulazeezlawal236@gmail.com>
2022-06-10 18:54:47 +0100

    my roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    hi = 0
-    boy = 0
-    roman1 = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    for i in roman_string[::-1]:
-        hello = roman1[i]
-        if hello >= hi:
-            boy = boy + hello
-            hi = hello
-        else:
-            boy = boy - hello
-    return boy
+    if isinstance(roman_string, str):
+        hi = 0
+        boy = 0
+        roman1 = {"I": 1, "V": 5, "X": 10,
+                  "L": 50, "C": 100, "D": 500, "M": 1000}
+        for i in roman_string[::-1]:
+            hello = roman1[i]
+            if hello >= hi:
+                boy = boy + hello
+                hi = hello
+            else:
+                boy = boy - hello
+        return (boy)
+    else:
+        return (0)
