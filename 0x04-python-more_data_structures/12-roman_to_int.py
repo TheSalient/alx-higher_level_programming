7dcb48e203630475d49e635b46f3922f033892fe
TheSalient <abdulazeezlawal236@gmail.com>
2022-06-10 18:47:31 +0100

    my roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,36 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     hi = 0
-    i = 0
-    while (i < len(roman_number)):
-        s1 = value(roman_number[i])
-        if (i + 1 < len(roman_number)):
-            s2 = value(roman_number[i + 1])
-            if (s1 >= s2):
-                hi += s1
-                i += 1
-            else:
-                hi += s2 - s1
-                i += 2
+    boy = 0
+    roman1 = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    for i in roman_string[::-1]:
+        hello = roman1[i]
+        if hello >= hi:
+            boy = boy + hello
+            hi = hello
         else:
-            hi += s1
-            i += 1
-    return hi
-
-
-def value(s):
-    if (s == 'I'):
-        return 1
-    if (s == 'V'):
-        return 5
-    if (s == 'X'):
-        return 10
-    if (s == 'L'):
-        return 50
-    if (s == 'C'):
-        return 100
-    if (s == 'D'):
-        return 500
-    if (s == 'M'):
-        return 1000
-    return 0
+            boy = boy - hello
+    return boy
