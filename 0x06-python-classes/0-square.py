4f262ee8ca284d8ab53844b35b7cb563fc9fb346
TheSalient <abdulazeezlawal236@gmail.com>
2022-06-22 14:55:37 +0100

    hi

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""""creating a square class."""
+
+
 class Square:
-    """"creating a square class."""
+    """"creating a square class. This is an empty class
+    """
     pass
