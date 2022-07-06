928988b272de311ec41e63245b4a5be3657da31d
TheSalient <abdulazeezlawal236@gmail.com>
2022-07-06 15:33:48 +0100

    i/o files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,5 @@ function that reads a text file (UTF8) and prints it to stdout:
 def read_file(filename=""):
     """real file."""
     with open("filename", encoding="utf-8") as f:
-        print(f.read())
+        for line in f:
+            print(line, end="")
