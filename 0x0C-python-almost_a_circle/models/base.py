40f8a5123e7a69a08e761ad0826eca6b733f5abe
TheSalient <abdulazeezlawal236@gmail.com>
2022-07-13 11:21:53 +0100

    hi

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+the class Base
+"""
+
+
+class Base:
+    """The function define"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
